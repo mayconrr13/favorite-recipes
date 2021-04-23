03a34a992540e868281ba8a1a22995c92e7c9bb8
Maycon Reis Rosário <mayconrr13@gmail.com>
2021-04-23 19:33:15 -0300

    eslint + prettier + editorconfig

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
-import { Provider } from 'next-auth/client'
-
-import { GlobalStyle } from '../styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider session={pageProps.session}>
-      <GlobalStyle />
+    < >
+      {/* <GlobalStyle /> */}
       <Component {...pageProps} />
-      </Provider>
+    </>
   )
 }
 
